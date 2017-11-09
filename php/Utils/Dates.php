f35f43472f5beb22faff046ac6ae3ea721ea9f87
Igor Duda <igor.duda@probegin.c>
2017-11-09 20:06:39 +0200

    Initial commit

---
+++ php/Utils/Dates.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Utils;
+
+class Dates
+{
+    /**
+     * Get number of days in a given quarter
+     *
+     * @param  int $year    Year to consider
+     * @param  int $quarter Quarter to consider
+     * @return int Number of days
+     * @throws \InvalidArgumentException
+    */
+    public static function getDaysInQuarter(int $year, int $quarter): int
+    {
+        // @TODO: create body of this function and cover with tests
+    }
+
+}
+
